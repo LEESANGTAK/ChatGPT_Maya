4aba06784c3aed6586e0313225a1a95eb6daff4d
Tak <chst27@gmail.com>
2024-05-02 07:42:46 +0900

    added remarks(comments) to the install batch scripts

--- install/install_maya2024_win.bat
+++ install/install_maya2024_win.bat
@@ -1,5 +1,18 @@
 @echo off
 
+REM The code `set MayaVersion=2024` is a batch script command used to create or update a variable named `MayaVersion` with the value `2024`.
+REM
+REM Explanation:
+REM - `set`: This keyword is used in batch scripting to assign a value to a variable.
+REM - `MayaVersion`: This is the name of the variable being created or updated.
+REM - `2024`: This is the value assigned to the variable `MayaVersion`.
+REM
+REM Functionality:
+REM - When this code is executed in a batch script, it creates a variable named `MayaVersion` if it does not already exist, and assigns the value `2024` to it.
+REM - If the `MayaVersion` variable already exists, running this command will update its value to `2024`.
+REM
+REM Example:
+REM If you run this code in a batch script, the variable `MayaVersion` will be created or updated with the value `2024`. Subsequently, you can reference this variable throughout the script to access or modify the Maya version number.
 set MayaVersion=2024
 
 call %~dp0install_win.bat
--- install/install_win.bat
+++ install/install_win.bat
@@ -1,8 +1,43 @@
 set ModuleName=chatmaya
 
+REM The code snippet `%~dp0` is a special parameter expansion used in batch scripting to retrieve the drive letter and path of the current batch script file.
+REM
+REM Explanation:
+REM - `%0` represents the current batch script file.
+REM - `%~dp` is a combination of special characters that perform specific actions on the parameter.
+REM - `%~d` extracts the drive letter of the specified parameter.
+REM - `%~p` extracts the path of the specified parameter.
+REM
+REM Therefore, `%~dp0` combines these expansions to provide the drive letter and path of the current batch script file.
+REM
+REM Example:
+REM If the batch script file is located at "C:\Scripts\example.bat", `%~dp0` will output "C:\Scripts\".
 set CurrentPath=%~dp0
+
+REM The code snippet `%CurrentPath:~0,-2%` is a substring extraction operation used in batch scripting to manipulate the value of the variable `CurrentPath`.
+REM
+REM Explanation:
+REM - `%CurrentPath%` represents the variable containing a path or string.
+REM - `:~0,-2` is a substring extraction syntax that specifies which part of the variable to extract.
+REM
+REM Breakdown of `:~0,-2`:
+REM - `:~` indicates that a substring operation is being performed.
+REM - `0` specifies the starting index of the substring. In this case, it starts from the beginning of the string (index 0).
+REM - `-2` specifies the length of the substring to extract. The negative sign indicates counting from the end of the string, and `-2` means to exclude the last two characters.
+REM
+REM Functionality:
+REM - `%CurrentPath:~0,-2%` will extract a substring from `CurrentPath` starting from the beginning and excluding the last two characters.
+REM
+REM Example:
+REM If `CurrentPath` contains the value "C:\Folder\Subfolder\", `%CurrentPath:~0,-2%` will output "C:\Folder\Subfolder" by excluding the last two characters, which are the backslash and the character before it.
 set A=%CurrentPath:~0,-2%
-for %%A in (%A%) do set RootPath=%%~dpA
+
+REM This script extracts the drive and path of the items in variable %A
+REM and stores it in the variable RootPath
+for %%A in (%A%) do (
+    REM For each item in %A, iterate and set RootPath to the drive and path
+    set RootPath=%%~dpA
+)
 
 set MayaPy="%ProgramFiles%\Autodesk\Maya%MayaVersion%\bin\mayapy.exe"
 
